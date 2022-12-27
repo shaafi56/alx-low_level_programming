4b2c0d16e509806d29da1b6a4e3cdf7df7606e4c
shaafi56 <shaafiaadan12@gmail.com>
2022-12-27 05:35:45 -0800

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - program that prints the lowercase alphabet in reverse.
+ * main - program that prints all single digit numbers of base 10 starting from
+ * You are not allowed to use any variable of type char
  * You can only use the putchar function
  * Return: 0
  */
 
 int main(void)
 {
-int n = 122;
-while (n > 96)
+int n = 48;
+while (n <= 57)
 {
 putchar(n);
-n -= 1;
+n += 1;
 }
-putchar('\n');
+printf("\n");
 return (0);
 }
